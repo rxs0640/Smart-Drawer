48dcb770ef4d2ff880c6b1b4970f79e1c1b6c2fa
pc <pc@MacBook-Air.home>
2016-12-23 03:33:09 -0800

    Initial commit

---
+++ Smart-Drawer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smart-Drawer
+//
+//  Created by pc on 12/23/16.
+//  Copyright © 2016 Sr. Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
