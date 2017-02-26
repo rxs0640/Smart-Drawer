ce0c40e086a2879b77841cbd4a26228376ae5ebb
rxs0640 <admin@Lances-MBP.home>
2017-02-25 18:07:46 -0800

    Added frameworks

--- deps/BLEFramework/BLE/BLE.m
+++ deps/BLEFramework/BLE/BLE.m
@@ -1,8 +1,6 @@
 
 /*
  
- BLE framework source code is placed under the MIT license 
-
  Copyright (c) 2013 RedBearLab
  
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -203,8 +201,6 @@ static int rssi = 0;
 
 - (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error;
 {
-    done = false;
-
     [[self delegate] bleDidDisconnect];
     
     isConnected = false;
@@ -458,10 +454,7 @@ static int rssi = 0;
     
     self.activePeripheral = peripheral;
     [self.activePeripheral discoverServices:nil];
-    [self getAllServicesFromPeripheral:peripheral];
 }
-
-static bool done = false;
 
 - (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error
 {
@@ -477,13 +470,9 @@ static bool done = false;
             
             if ([service.UUID isEqual:s.UUID])
             {
-                if (!done)
-                {
-                    [self enableReadNotification:activePeripheral];
-                    [[self delegate] bleDidConnect];
-                    isConnected = true;
-                    done = true;
-                }
+                [self enableReadNotification:activePeripheral];
+                [[self delegate] bleDidConnect];
+                isConnected = true;
                 
                 break;
             }
